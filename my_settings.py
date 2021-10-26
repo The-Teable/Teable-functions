2d8224b63320b900319982c555ecf2b3697a2d36
hyun <zkdlxkxks@nate.com>
2021-10-27 02:01:26 +0900

    change: host: localhost -> 127.0.0.1

--- my_settings.py
+++ my_settings.py
@@ -7,7 +7,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'SkaksdmlTea',
         # 'HOST': '/opt/bitnami/mysql/tmp/mysql.sock',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '1234',
     }
 }
