3a18b03df9b3afa1849b92fb0ba828d2f24db1ef
dyun <zkdlxkxks@nate.com>
2022-06-15 13:07:03 +0900

    change : rename, db connect
    file name, colum name, db connect

--- apis/lib/common_filtering.py
+++ apis/lib/common_filtering.py
@@ -9,14 +9,13 @@ import random
 # import sklearn
 # from sklearn.model_selection import train_test_split
 # from sklearn.metrics.pairwise import cosine_similarity
-
+from .ConnectDB import MYSQLDB
 # class FilteringAlgorithm:
 
 
 def tea_filtering(user_teatype, user_scent, user_effect, user_caff):
-    tea_db = (
-        pd.read_csv(('apis/lib/assets/Tea DB.csv'), encoding='utf-8')
-    )
+    myDB = MYSQLDB()
+    tea_db = myDB.teas
     df = pd.DataFrame(tea_db)
 
     def type_filter(teatypes):
@@ -66,21 +65,23 @@ def tea_filtering(user_teatype, user_scent, user_effect, user_caff):
             return True
         else:
             return False
-    df.rename(columns={'맛,향(필터링)': '맛'}, inplace=True)
-    df.rename(columns={'효능(필터링)': '효능'}, inplace=True)
-    df.rename(columns={'카페인 여부': '카페인'}, inplace=True)
-    df.rename(columns={'차 종류': '종류'}, inplace=True)
-    df.맛.str.split(',')
-    df.효능.str.split(',')
-    df.종류.str.split('.')
+    # df.rename(columns={'맛,향(필터링)': '맛'}, inplace=True)
+    # df.rename(columns={'효능(필터링)': '효능'}, inplace=True)
+    # df.rename(columns={'카페인 여부': '카페인'}, inplace=True)
+    # df.rename(columns={'차 종류': '종류'}, inplace=True)
+    df.rename(columns={'name': 'tea_name'}, inplace=True)
+    df.rename(columns={'brand': 'tea_brand'}, inplace=True)
+    df.flavor.str.split(',')
+    df.efficacies.str.split(',')
+    df.type.str.split('.')
     # df.카페인.apply(caff_filter).sum()
     # df.효능.apply(effect_filter).sum()
     # df.맛.apply(scent_filter).sum()
     # df.종류.apply(type_filter).sum()
-    cond_type = df.종류.apply(type_filter)
-    cond_effect = df.효능.apply(effect_filter)
-    cond_scent = df.맛.apply(scent_filter)
-    cond_caff = df.카페인.apply(caff_filter)
+    cond_type = df.type.apply(type_filter)
+    cond_effect = df.efficacies.apply(effect_filter)
+    cond_scent = df.flavor.apply(scent_filter)
+    cond_caff = df.caffeine.apply(caff_filter)
     user_finalise = df[cond_type & cond_effect & cond_scent & cond_caff]
 
     if len(user_finalise) > 8:
@@ -90,42 +91,42 @@ def tea_filtering(user_teatype, user_scent, user_effect, user_caff):
             frac=1).reset_index(drop=True).head(8)
     else:
         user_finalise = user_finalise.append(
-            df[cond_type & cond_scent & cond_caff]).drop_duplicates(['제품 이름'])
+            df[cond_type & cond_scent & cond_caff]).drop_duplicates(['tea_name'])
         # user_finalise = user_finalise.sort_values(
         #     by='재고', ascending=False).head(8)
         user_finalise = user_finalise.sample(
             frac=1).reset_index(drop=True).head(8)
         if len(user_finalise) < 8:
             user_finalise = user_finalise.append(
-                df[cond_type & cond_effect & cond_caff]).drop_duplicates(['제품 이름'])
+                df[cond_type & cond_effect & cond_caff]).drop_duplicates(['tea_name'])
             # user_finalise = user_finalise.sort_values(
             #     by='재고', ascending=False).head(8)
             user_finalise = user_finalise.sample(
                 frac=1).reset_index(drop=True).head(8)
             if len(user_finalise) < 8:
                 user_finalise = user_finalise.append(
-                    df[cond_type & cond_caff]).drop_duplicates(['제품 이름'])
+                    df[cond_type & cond_caff]).drop_duplicates(['tea_name'])
                 # user_finalise = user_finalise.sort_values(
                 #     by='재고', ascending=False).head(8)
                 user_finalise = user_finalise.sample(
                     frac=1).reset_index(drop=True).head(8)
                 if len(user_finalise) < 8:
                     user_finalise = user_finalise.append(
-                        df[cond_type & cond_scent]).drop_duplicates(['제품 이름'])
+                        df[cond_type & cond_scent]).drop_duplicates(['tea_name'])
                     # user_finalise = user_finalise.sort_values(
                     #     by='재고', ascending=False).head(8)
                     user_finalise = user_finalise.sample(
                         frac=1).reset_index(drop=True).head(8)
                     if len(user_finalise) < 8:
                         user_finalise = user_finalise.append(
-                            df[cond_type & cond_effect]).drop_duplicates(['제품 이름'])
+                            df[cond_type & cond_effect]).drop_duplicates(['tea_name'])
                         # user_finalise = user_finalise.sort_values(
                         #     by='재고', ascending=False).head(8)
                         user_finalise = user_finalise.sample(
                             frac=1).reset_index(drop=True).head(8)
                         if len(user_finalise) < 8:
                             user_finalise = user_finalise.append(
-                                df[cond_type]).drop_duplicates(['제품 이름'])
+                                df[cond_type]).drop_duplicates(['tea_name'])
                             # user_finalise = user_finalise.sort_values(
                             #     by='재고', ascending=False).head(8)
                             user_finalise = user_finalise.sample(
