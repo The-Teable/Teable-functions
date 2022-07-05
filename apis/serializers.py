293c1e5f4001dad97af3092e762cb8cb63d84036
dyun <zkdlxkxks@nate.com>
2022-07-06 00:41:11 +0900

    init : auth

--- apis/serializers.py
+++ apis/serializers.py
@@ -6,6 +6,51 @@ from .models import FilteringResultProductMap, FilteringResults, Questionnaires,
 from .lib import common_filtering, teave_filtering
 import json
 
+#django auth
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+#auth serializer
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        # ...
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
 class TeaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teas
