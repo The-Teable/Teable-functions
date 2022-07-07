c2191a549008d5feb2b450888a71c158bc3d6b51
dyun <zkdlxkxks@nate.com>
2022-07-08 00:56:53 +0900

    add: get_user_click

--- apis/lib/ConnectDB.py
+++ apis/lib/ConnectDB.py
@@ -6,16 +6,17 @@ import pymysql
 import pandas as pd
 
 # db연결
+
 class MYSQLDB:
     def __init__(self):
-        """생성자: MySQL 연결 및 종목코드 딕셔너리 생성"""
+        """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
         self.conn = pymysql.connect(host='localhost', user='root',
                                     password='SkaksdmlTea', db='teable_dev', charset='utf8')
         self.users = self.get_user()
         self.teas = self.get_content()
         self.recommend_test_result = self.get_recommend_test_result()
         self.user_buy_df = self.get_user_buy()
-        self.test_boolean = self.get_recommend_test()
+        self.user_click_df = self.get_user_click()
 
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
@@ -36,14 +37,6 @@ class MYSQLDB:
         tea_df = pd.DataFrame(tea_info)
         return tea_df
 
-    def get_recommend_test(self):
-        """recommend test 여부를 test_boolean에 저장"""
-        sql = "SELECT * FROM filtering_results"
-        test_info = pd.read_sql(sql, self.conn)
-        test_df = pd.DataFrame(test_info)
-        return test_df
-
-
     def get_recommend_test_result(self):
         """recommend test의 결과를 recommend_test_result에 저장"""
         sql = "SELECT * FROM filtering_result_product_map"
@@ -59,3 +52,11 @@ class MYSQLDB:
         user_buy_df = pd.DataFrame(user_buy_info)
         user_buy_df['interaction_type'] = 'Buy'
         return user_buy_df
+
+    def get_user_click(self):
+        """user_click_product를 가져와서 user_click_df에 저장"""
+        sql = "SELECT * FROM user_click_product"
+        user_click_info = pd.read_sql(sql, self.conn)
+        user_click_df = pd.DataFrame(user_click_info)
+        user_click_df['interaction_type'] = 'Click'
+        return user_click_df
