d2fb57d092c208a7f1a31c671e6b0efd3f60a8e7
dyun <zkdlxkxks@nate.com>
2022-07-20 02:22:49 +0900

    add: signup create url

--- apis/urls.py
+++ apis/urls.py
@@ -25,6 +25,10 @@ signup_check = views.SignUpView.as_view({
     'get': 'check_duplicate',
 })
 
+signup_create = views.SignUpView.as_view({
+    'post': 'create',
+})
+
 
 users_list = UsersView.as_view({
     'post': 'create',
@@ -109,12 +113,12 @@ send_email_list = SendEmail.as_view()
 
 urlpatterns = format_suffix_patterns([
 
-    # auth token
+    # auth login
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # auth signup
-    path('signup/', views.SignUpView.as_view(), name='auth_signup'),
+    path('signup/', signup_create, name='auth_signup'),
     path('signup/check/', signup_check, name = 'signup_check'),
     path('', views.getRoutes),
     path('', views.index),
