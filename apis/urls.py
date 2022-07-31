fbf451627cb35a0a7c5355332bbdbb7d01670bfa
dyun <zkdlxkxks@nate.com>
2022-07-31 16:50:58 +0900

    delete : api token/

--- apis/urls.py
+++ apis/urls.py
@@ -120,7 +120,7 @@ urlpatterns = format_suffix_patterns([
     path('', views.index, name = 'main'),
 
     # auth token
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
